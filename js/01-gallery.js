b234c51bda434b5db804da24a8239f7f4a37d59b
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-10-10 16:11:46 +0300

    hw

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,99 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+console.log(galleryItems); 
+console.log(createGalleryItemMarkup(galleryItems));
+function createGalleryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+  })
+    .join('');
+ 
+}
+const galleryContainer = document.querySelector('.gallery');
+const galleryItemMarkup = createGalleryItemMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
 
-console.log(galleryItems);
+// function onGalleryContainerClick(evt) {
+//   evt.preventDefault();
+// const isGalleryEl =!evt.target.classList.contains('gallery__image')
+//   if (!isGalleryEl) {
+//     return;
+//   } else {
+//      const instance = basicLightbox.create(`
+//     <img src="${imgUrl}">`);
+//     instance.show();
+//     document.addEventListener('keydown', OnModalClose);
+//     instance.close()
+//     // const galleryEl = evt.target;
+//     // galleryEl.classList.add('.gallery__image')
+//     // console.log(evt.target.dataset.alt);
+//   }
+
+// }
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+const isGalleryEl = evt.target.classList.contains('.gallery__image')
+  if (!isGalleryEl) {
+    return;
+  }
+     const instance = basicLightbox.create(`
+    <img src="${imgUrl}">`);
+    instance.show();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function onOpenModal(evt) {
+//     // const instance = basicLightbox.create(`
+//     <img src="${imgUrl}">`);
+//     instance.show();
+//     document.addEventListener('keydown', OnModalClose);
+//     instance.close()
+// }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- js/gallery-items.js
+++ js/gallery-items.js
@@ -63,3 +63,24 @@ export const galleryItems = [
     description: 'Lighthouse Coast Sea',
   },
 ];
+
+
+
+// function createGalleryItemMarkup(galleryItems) {
+//   return galleryItems
+//     .map(({ preview, original, description }) => {
+//       return `
+//         <div class="gallery__item">
+//           <a class="gallery__link" href="${original}">
+//             <img
+//               class="gallery__image"
+//               src="${preview}"
+//               data-source="${original}"
+//               alt="${description}"
+//             />
+//           </a>
+//         </div>
+//       `;
+//     })
+//     .join('');
+// }
