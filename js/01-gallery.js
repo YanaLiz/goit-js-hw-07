8365cc7fb1ebae5e3043c34d7150371dcda3c714
YanaLiz <105737326+YanaLiz@users.noreply.github.com>
2022-10-10 21:39:26 +0300

    work

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -25,24 +25,7 @@ const galleryItemMarkup = createGalleryItemMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', galleryItemMarkup);
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 
-// function onGalleryContainerClick(evt) {
-//   evt.preventDefault();
-// const isGalleryEl =!evt.target.classList.contains('gallery__image')
-//   if (!isGalleryEl) {
-//     return;
-  
-//      const instance = basicLightbox.create(`
-//     <img src="${imgUrl}">`);
-//     instance.show();
-//     document.addEventListener('keydown', OnModalClose);
-//     instance.close()
 
-//   //   const galleryEl = evt.target;
-//   //   galleryEl.classList.add('.gallery__image')
-//   //   console.log(evt.target.dataset.alt);
-//   // }
-
-// }
 
 function onGalleryContainerClick(evt) {
   evt.preventDefault();
